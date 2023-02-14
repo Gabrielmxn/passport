15eb2989886ce12859f106f8026b2eba3e1ac3b4
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2023-02-14 16:44:44 -0300

    :sparkles: Mudança no tamanho da linha de corte

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -6,8 +6,9 @@ interface DividerProps {
 
 export function Divider({ passportValidation }: DividerProps) {
   return(
-    <div className={clsx("w-[calc(100%_-_(1.3rem_*_2))] border-dashed border-b-2 relative border-b-black/30 h-[2px] bg-white text-center mx-auto",{
-      'w-[calc(100%_-_(0.83rem_*_2))]': passportValidation
+    <div className={clsx("border-dashed border-b-2 relative border-b-black/30 h-[2px] bg-white text-center mx-auto",{
+      'w-[calc(100%_-_(1.4rem_*_2))]': !passportValidation,
+      'w-[calc(100%_-_(0.9rem_*_2))]': passportValidation
     })}>
     </div>
   )
